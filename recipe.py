d5a53753c09e8a6bb985b4db26b62ac988a073fe
Ajay Menon <ajay.menon.m@gmail.com>
2019-04-08 16:51:42 -0400

    add useful comments and move sort key to secrets yml

--- recipe.py
+++ recipe.py
@@ -13,6 +13,7 @@ class RecipeSearch:
         # self.api_key = os.environ['KEY'] or config['KEY']
         # TODO add env
         self.api_key = config['KEY']
+        self.sort_by = config['SORT_TYPE']
         self.bulk_recipe_url = config['RECIPE_URL']
         self.specific_recipe_url = config['INGREDIENT_URL']
         self.table = {}
@@ -25,6 +26,7 @@ class RecipeSearch:
                 generic_instruction = '\n\n    Instructions: \n **Enter 1 to exit.** '
                 print(generic_instruction if len(self.ingredients_with_user) == 0 else generic_instruction + '\n **Enter 2 to search for recipe.**')
 
+                # get ingredients from user
                 user_input = input('Please Enter Ingredient No.{0} \n'.format(len(self.ingredients_with_user) + 1))
                 user_input = user_input.replace(' ', '').lower()
 
@@ -37,6 +39,13 @@ class RecipeSearch:
                     self.search_popular_recipes()
                     return
 
+                """
+                Entered Ingredients Acceptance Criteria:
+                1. Only alphabets
+                2. No whitespaces
+                3. No special characters
+                4. Ingredients entered one at a time
+                """
                 if user_input.isalpha():
                     self.ingredients_with_user.append(user_input)
                     self.ingredients_with_user = list(set(self.ingredients_with_user))
@@ -53,10 +62,11 @@ class RecipeSearch:
             ingredients = ','.join(self.ingredients_with_user)
             query_params = {
                 'key': self.api_key,
-                'sort': 'r',
+                'sort': self.sort_by,
                 'q': ingredients
             }
 
+            # api request to get matching recipe list
             api_request = requests.get(self.bulk_recipe_url, params=query_params)
             data = api_request.json()
 
@@ -68,33 +78,29 @@ class RecipeSearch:
                 already_available_ingredients = set()
                 missing_ingredients = set()
                 matching_ingredients = set()
+                current_recipe = []
 
                 for item in range(data['count']):
 
                     length_with_user = len(self.ingredients_with_user)
                     length_available = len(already_available_ingredients)
 
-                    # print('length of available ==========', length_available)
-                    # print('length of ing with user ==========', length_with_user)
-
-                    '''
+                    """
                     Return Missing Ingredients 
                     from the Most Popular Recipe
-                    '''
+                    """
                     if length_available == length_with_user:
-                        print('\nMissing Ingredients:')
-                        print("\n".join(missing_ingredients))
+                        display_result(current_recipe, already_available_ingredients, missing_ingredients)
                         return
 
                     recipe_id = data['recipes'][item]['recipe_id']
-
                     query_params = {
                         'key': self.api_key,
                         'rId': recipe_id
                     }
 
+                    #api request to get individual recipe ingredients
                     api_request = requests.get(self.specific_recipe_url, params=query_params)
-                    # print(api_request.json())
 
                     # clear data from previous iteration
                     full_ingredients.clear()
@@ -102,14 +108,14 @@ class RecipeSearch:
                     missing_ingredients.clear()
                     matching_ingredients.clear()
 
+                    # parse api response
                     api_data = api_request.json()
+                    current_recipe = api_data['recipe']
                     full_ingredients = api_data['recipe']['ingredients']
-
-                    # self.searchIndividualRecipeIngredients(recipe_id)
                     full_ingredients = [x.lower() for x in full_ingredients]
                     full_ingredients = set(full_ingredients)
-                    # for ingredient in self.ingredients_with_user:
-                    # TODO: Refactor this block
+
+                    #search missing ingredient
                     for x in self.ingredients_with_user:
                         for y in full_ingredients:
                             if x in y:
@@ -118,7 +124,6 @@ class RecipeSearch:
                             else:
                                 missing_ingredients.add(y)
                     missing_ingredients = full_ingredients - matching_ingredients
-
 
                     # for ingredient in full_ingredients:
                     #     # print(s.lower() in ingredient for s in self.ingredients_with_user)
@@ -134,51 +139,40 @@ class RecipeSearch:
                     self.table[recipe_id]['available_ingredients'] = already_available_ingredients
                     self.table[recipe_id]['missing_ingredients'] = missing_ingredients
 
-                    print('available  ======= {0}'.format(already_available_ingredients))
-                    print('available items from recipe  ======= {0}'.format(matching_ingredients))
-                    print('missing ========= {0}'.format(missing_ingredients))
-                    print('table ========== {0}'.format(self.table))
-                    # self.getIngredients()
 
-                # returns highest matching one
-                print('Missing Ingredients: \n')
+                """
+                If not all of the ingredients entered by User found in the recipe (all individual recipes),
+                return the most popular / rated with highest matching ingredient.
+                """
                 for item in list(self.table):
-                    print('\n'.join(self.table[item]['missing_ingredients']))
+                    display_result(current_recipe, already_available_ingredients, self.table[item]['missing_ingredients'])
                     break
-
             self.get_ingredients()
-        # return
+
         except Exception:
             logger.error('Exception Raised: ', exc_info=True)
-
-    # def searchIndividualRecipeIngredients(self, recipe_id):
-    #
-    #     self.full_ingredients = map(lambda x:x.lower(), self.full_ingredients)
-    #     for ingredient in self.full_ingredients:
-    #         # print(ingredient)
-    #         if any(s.lower() in ingredient for s in self.ingredients_with_user):
-    #             self.available.append(ingredient)
-    #         else:
-    #             self.missing_ingredients.append(ingredient)
-    #
-    #     self.table[recipe_id] = {}
-    #     self.table[recipe_id]['available_length'] = len(self.available)
-    #     self.table[recipe_id]['available_ingredients'] = self.available
-    #     self.table[recipe_id]['missing_ingredients'] = self.missing_ingredients
-    #
-    #     print('available {0}'.format(self.available))
-    #     print('missing {0}'.format(self.missing_ingredients))
-    #     print('table {0}'.format(self.table))
-    #     return
 
     def __clear_data(self):
         self.ingredients_with_user.clear()
         self.table.clear()
 
-    def __load_secrets(self):
+    @staticmethod
+    def __load_secrets():
         with open("secrets.yml", 'r') as ymlfile:
             config = yaml.load(ymlfile)
         return config['API_VARIABLES']
+
+# utility method to display result
+def display_result(current_recipe, already_available_ingredients, missing_ingredients):
+    print('\n######################################################################')
+    print('\n**{0} is the Most Popular Recipe I could find for the ingredients you have.**'.format(
+        current_recipe['title']))
+    print('Title: {0}\nAvailable Ingredients: {1}'.format(current_recipe['title'],
+                                                          ', '.join(already_available_ingredients)))
+    print('\nMissing Ingredients:')
+    print('\n'.join(missing_ingredients))
+    print('\nFor Cooking Instructions, Visit: {0}'.format(current_recipe['source_url']))
+    print('\n######################################################################')
 
 
 rs = RecipeSearch()
